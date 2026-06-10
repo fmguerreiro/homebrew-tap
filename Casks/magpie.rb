1ddaad9874fd84805381fb11c93f793e872a9a67
filipeGuerreiro <filipe.m.guerreiro@gmail.com>
2026-06-10 10:56:42 +0900

    magpie 0.1.0

--- Casks/magpie.rb
+++ Casks/magpie.rb
@@ -1,6 +1,6 @@
 cask "magpie" do
   version "0.1.0"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "3ac2cd321c4a41585c0a65cacd4de99fcaac5633430848431c092650b7fa03c9"
 
   url "https://github.com/fmguerreiro/magpie/releases/download/v#{version}/Magpie.dmg"
   name "Magpie"
